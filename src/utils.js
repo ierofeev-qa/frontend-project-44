b12c4e4a70caa4a2c48919c559386916e9dd8874
ierofeev <ivanerofeev@bk.ru>
2024-02-16 01:03:16 +0400

    Fix lint

--- src/utils.js
+++ src/utils.js
@@ -18,10 +18,10 @@ const gcd = (a, b) => {
 };
 
 const isPrimeNumber = (number) => {
-    for (let i = 2; i < number; i += 1) {
-      if (number % i === 0 || number < 2) return false;
-    }
-    return true;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0 || number < 2) return false;
+  }
+  return true;
 };
 
 const evenGameData = () => {
